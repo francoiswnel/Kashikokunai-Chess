0006bfc3356f706aa7f3ed1597798c14bd4e9c98
Francois W. Nel <francoiswnel@protonmail.com>
2017-12-27 15:42:24 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Kashikokunai Chess
+ * Created by Francois W. Nel on 27 Dec 2017.
+ *
+ * Description:
+ *  TODO
+ *
+ * Usage:
+ *  TODO
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
